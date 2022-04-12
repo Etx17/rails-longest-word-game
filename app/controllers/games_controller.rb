ddf6870248e9b1ea8d6e8a98af1416c5d74ff512
Etienne de Dianous <etiennededi@hotmail.fr>
2022-04-12 20:54:40 +0200

    Works fine with session score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,15 @@
 require 'json'
 require 'open-uri'
 class GamesController < ApplicationController
+  # session[:points] = 0
+
   def new
+    if session[:points]
+      @points = session[:points]
+    else
+      session[:points] = 0
+      @points = session[:points]
+    end
     @letters = []
     10.times { @letters << rand(65..90).chr }
   end
@@ -11,11 +19,18 @@ class GamesController < ApplicationController
     @word_submitted = params[:word].upcase
     @word_is_valid = word_is_valid(@word_submitted, @letters)
     @word_is_english = word_is_english(@word_submitted)
+    @points = session[:points]
+
+    if @word_is_english
+     session[:points] += @word_submitted.length
+     @points = session[:points]
+    end
   end
 
   def word_is_valid(word, letters)
     word = word.split('')
     arr_letters = letters.split('')
+
     word.each do |letter|
       if arr_letters.include?(letter)
         arr_letters.delete_at(arr_letters.find_index(letter))
@@ -31,5 +46,11 @@ class GamesController < ApplicationController
     word_serialized = URI.open(url).read
     result = JSON.parse(word_serialized)
     result["found"]
+  end
+
+  private
+
+  def set_session
+    session[:points] = 0
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'logins/create'
   get '/new', to: 'games#new'
   post '/score', to: 'games#score'
 end
